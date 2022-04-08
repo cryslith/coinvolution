f7fee1a65e0d5ae60332cdd1ea11cede757cc9e3
Lily Chung <lkdc@mit.edu>
2022-04-07 21:09:00 -0400

    add shell for convexification

---
+++ folding/src/convex.rs
@@ -0,0 +1,7 @@
+use gmap::{Dart, GMap, OrbitMap};
+use na::Point2;
+
+/// Y-monotone partition the specified face of g.
+pub fn y_monotone_partition(g: &GMap, vertices_coords: OrbitMap<Point2<f64>>, f: Dart) {
+  todo!()
+}
