83681ebd4622c0f4cb31c846153e20268b70735f
Lily Chung <lkdc@mit.edu>
2024-01-08 19:45:17 -0500

    sloop

--- python/src/pzp/__init__.py
+++ python/src/pzp/__init__.py
@@ -3,6 +3,7 @@ from ..gmap.grid import Grid
 from ..puzzle import Layer
 
 import re
+from itertools import chain
 
 def decode(s):
     m = re.match(r'(.*?p\?)?(.*?)/(.*?)/(.*?)/(.*)', s)
@@ -15,7 +16,9 @@ def decode(s):
     g = Grid(height, width)
     layers = []
     if variety == 'slither':
-        decoded = decode4Cell(height, width, data)
+        (decoded, data) = decode_4cell(height, width, data)
+        if data:
+            raise ValueError('extra data')
         layers = [
             Layer(
                 'clues',
@@ -23,13 +26,26 @@ def decode(s):
                 {g[y, x]: v for (y, r) in enumerate(decoded) for (x, v) in enumerate(r) if v >= 0},
             ),
         ]
+    elif variety == 'simpleloop':
+        (decoded, data) = decode_binary(height, width, data)
+        if data:
+            raise ValueError('extra data')
+        layers = [
+            Layer(
+                'shaded',
+                Alphas.FACE,
+                {g[y, x] for (y, r) in enumerate(decoded) for (x, v) in enumerate(r) if v},
+            ),
+        ]
     else:
         raise ValueError(f'unknown variety {variety}')
     return (variety, g, layers)
 
-def decode4Cell(height, width, data):
+def decode_4cell(height, width, data):
     r = []
-    for c in data:
+    for (rem, c) in enumerate(data):
+        if len(r) >= width * height:
+            break
         if '0' <= c <= '4':
             r.append(int(c, 16))
             continue
@@ -48,8 +64,27 @@ def decode4Cell(height, width, data):
             r.append(-2)
             continue
         raise ValueError(f'unrecognized character {c}')
+    else:
+        rem += 1
 
     if len(r) != width * height:
         raise ValueError(f"decoded length {len(r)} doesn't match dimensions {width=} {height=}")
 
-    return [r[i:i+width] for i in range(0, width*height, width)]
+    return [r[i:i+width] for i in range(0, width*height, width)], data[rem:]
+
+def decode_binary(height, width, data):
+    r = []
+    for (rem, c) in enumerate(data):
+        if len(r) >= width * height:
+            break
+        n = int(c, 32)
+        r.extend([((n >> i) & 1) == 1 for i in reversed(range(5))])
+    else:
+        rem += 1
+
+    r = r[:width*height]
+
+    if len(r) != width * height:
+        raise ValueError(f"decoded length {len(r)} doesn't match dimensions {width=} {height=}")
+
+    return [r[i:i+width] for i in range(0, width*height, width)], data[rem:]
--- python/src/pzp/test/__init__.py
+++ python/src/pzp/test/__init__.py
@@ -4,8 +4,7 @@ from .. import decode
 
 class TestPZP(TestCase):
     def test_decode_slither(self):
-        s = 'https://puzz.link/p?slither/7/4/01239.g56cgdjah'
-        (variety, g, layers) = decode(s)
+        (variety, g, layers) = decode('https://puzz.link/p?slither/7/4/01239.g56cgdjah')
         self.assertEqual(variety, 'slither')
         self.assertEqual(g.width, 7)
         self.assertEqual(g.height, 4)
@@ -24,5 +23,26 @@ class TestPZP(TestCase):
                 (1, 5, 2),
                 (2, 2, 3),
                 (3, 2, 0),
+            ]},
+        )
+
+    def test_decode_simpleloop(self):
+        (variety, g, layers) = decode('https://puzz.link/p?simpleloop/5/5/o2c1v')
+        [shaded] = layers
+        self.assertEqual(shaded.name, 'shaded')
+        self.assertEqual(
+            shaded.data,
+            {g[y, x] for (y, x) in [
+                (0, 0),
+                (0, 1),
+                (1, 3),
+                (2, 1),
+                (2, 2),
+                (3, 4),
+                (4, 0),
+                (4, 1),
+                (4, 2),
+                (4, 3),
+                (4, 4),
             ]},
         )
